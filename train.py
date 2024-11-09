8fb560597e270134c34eff69899d89d0c72334a3
Vasiliy <vasylyigorelov@gmail.com>
2024-11-09 20:05:38 +0300

    fix: clip

--- train.py
+++ train.py
@@ -579,7 +579,7 @@ if __name__ == "__main__":
 
     parser.add_argument("--image_dir", type=str, default="../sekrrno/dataset", help="image dir")
     parser.add_argument("--epochs_dir", type=str, default="./epochs_emb", help="epochs dir")
-    parser.add_argument("--embedding_size", type=int, default=128, help="embedding size")
+    parser.add_argument("--embedding_size", type=int, default=32, help="embedding size")
     parser.add_argument("--m_per_batch_size", type=int, default=8, help="m_per_batch_size")
     parser.add_argument("--batch_size", type=int, default=512, help="batch size")
     parser.add_argument("--load_last", type=bool, default=False, help="load last")
@@ -622,9 +622,9 @@ if __name__ == "__main__":
     train_labels = [dataset.data[idx]['label'] for idx in test_dataset.indices]
 
 
-    metric_loss = losses.TripletMarginLoss(margin=0.3, distance=CosineSimilarity())  
+    metric_loss = losses.TripletMarginLoss(margin=0.5, distance=CosineSimilarity())  
 
-    miner = miners.TripletMarginMiner(margin=0.8, type_of_triplets='semihard')
+    miner = miners.TripletMarginMiner(margin=0.3, type_of_triplets='semihard')
 
     sampler = MPerClassSampler(train_labels, m=args.m_per_batch_size, length_before_new_iter=len(train_labels))
 
