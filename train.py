39c9c437fdee5d076615c24e213c8818250dee29
Vasiliy <vasylyigorelov@gmail.com>
2024-11-09 16:34:39 +0300

    fix: clip

--- train.py
+++ train.py
@@ -579,8 +579,8 @@ if __name__ == "__main__":
 
     sampler = MPerClassSampler(train_labels, m=args.m_per_batch_size, length_before_new_iter=len(train_labels))
 
-    trunk_optimizer = torch.optim.Adam(trunk.parameters(), lr=5e-4, weight_decay=1e-5)
-    embedder_optimizer = torch.optim.Adam(embedder.parameters(), lr=5e-3, weight_decay=1e-5)
+    trunk_optimizer = torch.optim.Adam(trunk.parameters(), lr=5e-5, weight_decay=1e-5)
+    embedder_optimizer = torch.optim.Adam(embedder.parameters(), lr=e-3, weight_decay=1e-5)
 
     models = {'trunk': trunk, 'embedder': embedder}
     optimizers = {'trunk_optimizer': trunk_optimizer, 'embedder_optimizer': embedder_optimizer}
@@ -594,7 +594,7 @@ if __name__ == "__main__":
 
     classifier = nn.DataParallel(Classifier(embedding_dim=args.embedding_size, num_classes=num_classes)).to(device)  
 
-    classifier_optimizer = torch.optim.Adam(classifier.parameters(), lr=5e-3, weight_decay=1e-5)
+    classifier_optimizer = torch.optim.Adam(classifier.parameters(), lr=e-3, weight_decay=1e-5)
 
     optimizers['classifier_optimizer'] = classifier_optimizer
 
@@ -627,7 +627,7 @@ if __name__ == "__main__":
     knn_func = FaissKNN()
 
     accuracy_calculator = AccuracyCalculator(
-        include=('mean_average_precision_at_r',), k=10, knn_func=knn_func
+        include=('mean_average_precision',), k=10, knn_func=knn_func
     )
     tester = testers.GlobalEmbeddingSpaceTester(
         dataloader_num_workers=4,
