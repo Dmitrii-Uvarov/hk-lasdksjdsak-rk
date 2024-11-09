3411076ce0d75e2643907998350bf1f8238219c4
Vasiliy <vasylyigorelov@gmail.com>
2024-11-09 17:20:36 +0300

    fix: clip

--- captioning.sh
+++ captioning.sh
@@ -1,1 +1,1 @@
-python script.py --image_dir "/kaggle/input/sekrrno/dataset" --output_file "captions.json"
+python captioning.py --image_dir "/kaggle/input/sekrrno/dataset" --output_file "captions.json"
